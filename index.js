4f51ac6b7e7e21174eafbadf51708cfd5e8d18ee
Ajayb <ajaybiswalk@gmail.com>
2024-08-17 23:45:59 +0530

    Second Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from node API Server"); 
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        
+    }
+})
+
+
+mongoose.connect("mongodb+srv://ajaybiswalk:YKN03xiRNyZloSyL@backenddb.8zu7agb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Connected to the database");
+        app.listen(3000, () => {
+            console.log('server running on 3000');
+            
+        });
+    })
+    .catch(() => {
+    console.log("Connection failed!");
+    
+})
