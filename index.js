f4d87e0b6222fd1b43539312851c3ab8eca7f99e
Ajayb <ajaybiswalk@gmail.com>
2024-08-21 17:18:42 +0530

    Add code to the codespace

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require("./Database/database");
 const Product = require('./models/product.model');
+const productRoute = require("./routes/product.route");
 const app = express();
 
+//middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
 
 app.get('/', (req, res) => {
     res.send("Hello from node API Server"); 
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
 
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+db.then(() => {
+    console.log("Connected to the database");
+    app.listen(3000, () => {
+        console.log('server running on 3000');
         
-    }
+    });
 })
-
-
-mongoose.connect("mongodb+srv://ajaybiswalk:YKN03xiRNyZloSyL@backenddb.8zu7agb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
-    .then(() => {
-        console.log("Connected to the database");
-        app.listen(3000, () => {
-            console.log('server running on 3000');
-            
-        });
-    })
-    .catch(() => {
+.catch(() => {
     console.log("Connection failed!");
-    
-})
+
+});
